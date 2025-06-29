9520fed9d83564fa19cc010d9aa6dc156958e21d
EmanoelMittmann <emanoelmittmann@hotmail.com>
2025-06-28 23:24:11 -0300

    fix: change-redis-connection

--- src/infrastructure/redis/options.ts
+++ src/infrastructure/redis/options.ts
@@ -1,21 +1,18 @@
 import { FactoryProvider, Logger } from '@nestjs/common';
 import { Redis } from 'ioredis';
 
-const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
-const REDIS_USER = process.env.REDIS_USER || 'guest'
-const REDIS_PASS = process.env.REDIS_PASS || 'guest'
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
 
 export const REDIS_CLIENT_FACTORY: FactoryProvider<Redis> = {
   provide: 'RedisClient',
   useFactory: () => {
     const logger = new Logger('REDIS_CLIENT_FACTORY');
-    const client = new Redis({
-      host: REDIS_HOST,
-      port: +REDIS_PORT,
-      username: REDIS_USER,
-      password: REDIS_PASS
-    });
+
+    if (!REDIS_URL) {
+      throw new Error('REDIS_URL is not defined');
+    }
+
+    const client = new Redis(REDIS_URL);
 
     client.on('error', (error) => {
       logger.error('Redis client error', error);
